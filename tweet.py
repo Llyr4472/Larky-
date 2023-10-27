2110b8b16d9f73ffd31f61bb21658f158e3c443b
Llŷr <mr.prashant2077@gmail.com>
2023-10-27 14:38:56 +0545

    basic implementation

--- tweet.py
+++ tweet.py
@@ -1,12 +1,16 @@
 import tweepy
 import time
 import pyjokes
+import configparser
 
-# Define your Twitter API credentials
-consumer_key = "1PZ6foLWh9GFvPqd69sja4ju6"
-consumer_secret = "54WVkKgSPTqBciq34yYwEMN3GCISE4yQ336VuspLemPlXAzVHH"
-access_token = "1717808070456381440-3un3LbTzfneVKn7cUXvwDYLZbT3OtZ"
-access_token_secret = "s2wpWuvvrhK6r2q6KVQ7NnsfNHgcN4H3GddKtIOPVKNGc"
+# Read credentials from config
+config = configparser.ConfigParser()
+config.read("config.ini")
+credentials = config["Credentials"]
+consumer_key = credentials["consumer_key"]
+consumer_secret = credentials["consumer_secret"]
+access_token = credentials["access_token"]
+access_token_secret = credentials["access_token_secret"]
 
 # Initialize Tweepy with your API credentials
 client = tweepy.Client(
