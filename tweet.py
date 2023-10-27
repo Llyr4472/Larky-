6be330114db28ce8417a2a70e19cad9da7190fba
Llŷr <mr.prashant2077@gmail.com>
2023-10-27 16:22:17 +0545

    Update tweet.py
    implemented memes

--- tweet.py
+++ tweet.py
@@ -1,7 +1,8 @@
+import os
 import tweepy
-import time
 import pyjokes
 import configparser
+import requests
 
 # Read credentials from config
 config = configparser.ConfigParser()
@@ -18,14 +19,39 @@ client = tweepy.Client(
     access_token=access_token, access_token_secret=access_token_secret
 )
 
-# Function to generate humor content
-def content():
-    # Implement content generation logic here
-    return pyjokes.get_joke()
+auth = tweepy.OAuth1UserHandler(
+    consumer_key, consumer_secret, access_token, access_token_secret
+)
 
-def tweet():
-    response = client.create_tweet(text=content())
-    print(f"https://twitter.com/user/status/{response.data['id']}") 
+api =tweepy.API(auth)
 
-tweet()
+def joke():
+    joke= pyjokes.get_joke()
+    response = client.create_tweet(text=joke)
+    print(f"Tweet posted: https://twitter.com/user/status/{response.data['id']}")
+
+def meme():
+    meme_api_url = "https://meme-api.com/gimme"
+    response = requests.get(meme_api_url)
+    if response.status_code == 200:
+        url= response.json()["url"]
+    else:
+        return None
+    
+    #save the meme
+    image = requests.get(url).content
+    with open('meme_temp.jpg', 'wb') as handler:
+        handler.write(image)
+    
+    #get media id
+    media_id = api.media_upload(filename="meme_temp.jpg").media_id
+    
+    #tweet
+    response = client.create_tweet(media_ids=[media_id])
+    print(f"Tweet posted: https://twitter.com/user/status/{response.data['id']}")
+    
+    #get rid of meme file
+    os.remove("meme_temp.jpg")
+
+meme()
 # End of the script
